0c27548cddf7afe3ee80155972a72793404ded44
bdodd1 <160481247+bdodd1@users.noreply.github.com>
2024-03-20 16:49:23 +0000

    FCCCU_v5

--- FCCU_input.py
+++ FCCU_input.py
@@ -17,9 +17,10 @@ topology = {}
 
 topology['pipes'] = [('feed_air','comp_1') , ('comp_1','v_6') , ('v_6','react_1') , ('react_1','v_7') , ('v_7','prod_stack') , ('react_1','v_2') ,
                      ('v_2','react_2') , ('react_2','v_3') , ('v_3','react_1') , ('feed_feedstream','furn_1') , ('feed_fuel','v_1') , ('v_1','furn_1'),
-                     ('furn_1','react_2') , ('react_2','distil_1') , ('distil_1','prod_slurry') , ('distil_1','v_11') , ('v_11','prod_lightoil') , ('distil_1','v_10') , 
-                     ('v_10','prod_heavynaptha') , ('distil_1','flash_1') , ('flash_1','split_liq') , ('split_liq', 'v_8') , ('v_8','distil_1') , ('split_liq','v_9') , ('v_9','prod_lightnaptha') , 
-                     ('flash_1','split_gas') , ('split_gas', 'v_4') , ('v_4','comp_2') , ('comp_2','prod_lpg') , ('split_gas','prod_flare')]
+                     ('furn_1','react_2') , ('react_2','distil_1') , ('distil_1','prod_slurry') , ('distil_1','hex_1') , ('hex_1','distil_1') , ('feed_uthex1', 'hex_1') , 
+                     ('hex_1', 'prod_uthex1') , ('distil_1','v_11') , ('v_11','prod_lightoil') , ('distil_1','v_10') , ('v_10','prod_heavynaptha') , ('distil_1','hex_2') , 
+                     ('hex_2','flash_1') , ('feed_uthex2', 'hex_1') , ('hex_1', 'prod_uthex2') , ('flash_1','split_liq') , ('split_liq', 'v_8') , ('v_8','distil_1') , ('split_liq','v_9') , 
+                     ('v_9','prod_lightnaptha') , ('flash_1','split_gas') , ('split_gas', 'v_4') , ('v_4','comp_2') , ('comp_2','prod_lpg') , ('split_gas','prod_flare')]
 
 topology['sensors'] = ['feed_air/comp_1.P_1', 'comp_1/v_6.F_7', 'comp_1/v_6.P_2', 'react_1.T_reg', 'react_1.L_sp', 'react_1.P_6' , 'react_1/v_7.F_sg', 
                        'react_1/v_7.T_cyc', 'v_7/prod_stack.X_co', 'v_7/prod_stack.X_co2', 'react_1/v_2.F_rgc', 'react_2.T_r', 'react_2.P_4', 
@@ -30,10 +31,6 @@ topology['sensors'] = ['feed_air/comp_1.P_1', 'comp_1/v_6.F_7', 'comp_1/v_6.P_2'
 topology['control_loops'] = {'v_6' : ['react_1.T_reg'] , 'v_1' : ['furn_1/react_2.T_2'] , 'v_3' : [('L', 'react_1')] , 'v_2' : ['react_2.T_r'] , 'v_7' : ['react_1.P_6'] , 'v_4' : ['distil_1.P_5'] , 
                              'v_9' : [('L', 'flash_1')] , 'v_8' : ['distil_1.T_fra'] , 'v_10' : ('T', 'distill_1') , 'v_11' : ('T', 'distill_1')}
 
-
-# topology['control_loops'] = {'T_C1' : ['furn_1/react_2.T_2', 'v_11'] , 'T_C2' : ['react_2.T_r', 'v_2'] , 'T_C3' : ['react_1.T_reg', 'v_6'] , 'L_C1' : ['react_2 ', 'v_3'] , 
-#                              'P_C1' : ['react_1.P_6', 'v_7'] , 'T_C6' : ['distil_1', 'v_11'] , 'T_C5' : ['distil_1', 'v_10'] , 'T_C4' : ['distil_1.T_fra', 'v_8'] , 
-#                              'P_C2' : ['distil_1.P_5', 'v_4'] , 'L_C2' : ['flash_1', 'v_9']}
 
 
 # Vars not being used in dataset 
@@ -66,7 +63,36 @@ configuration = {
                      'utility streams' : None,
                      'reactants' : None}, 
                 'flash_1' :
-                    {'gas stream' : 'flash_1/split_gas'}
+                    {'gas stream' : 'flash_1/split_gas'},
+                'distil_1' :
+                    {'total stages' : 20,
+                     'stream locations' :
+                        {'react_2/distil_1' : 20,
+                         'v_8/distil_1' : 0,
+                         'hex_1/distil_1' : 20,
+                         'distil_1/hex_2' : 0,
+                         'distil_1/hex_1' : 20,
+                         'distil_1/prod_slurry' : 20,
+                         'distil_1/v_11' : 17,
+                         'distil_1/v_10' : 8},
+                     'stream tags' :
+                        {'react_2/distil_1' : 'feed',
+                         'v_8/distil_1' : 'reflux',
+                         'hex_1/distil_1' : 'reboil',
+                         'distil_1/hex_2' : 'top',
+                         'distil_1/hex_1' : 'bot',
+                         'distil_1/prod_slurry' : 'prod',
+                         'distil_1/v_11' : 'prod',
+                         'distil_1/v_10' : 'prod'},
+                     'sensor locations' : 
+                        {'distil_1.T_fra' : 0,
+                         'distil_1.T_10' : 10,
+                         'distil_1.T_20' : 20,
+                         'distil_1.P_5' : 0}},
+                'hex_1' :
+                    {'process streams' : ['distil_1/hex_1', 'hex_1/distil_1']},
+                'hex_2' :
+                    {'process streams' : ['distil_1/hex_2', 'distil_1/flash_1']}
                 }
 
 
@@ -86,12 +112,5 @@ graph = unit_lib(topology, data, configuration, mode)
 # graph.model_comp()
 
 graph.build_graph()
-
-
-
-
-
-
-
 
 
